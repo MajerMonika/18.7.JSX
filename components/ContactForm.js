b8e1640d8c1d155671a4971b11078ac1d3e70765
Monika Majer <majer_monika@interia.pl>
2018-12-19 11:05:29 +0100

    syntax improvement in js files

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -5,20 +5,20 @@ var ContactForm = React.createClass({
 
   render: function () {
     return ( 
-      <form className = {'contactForm'}>
+      <form className={'contactForm'}>
         <input 
-          type = {'text'}
-          placeholder = {'Imię'}
-          value = {this.props.contact.firstName}/> 
+          type={'text'}
+          placeholder={'Imię'}
+          value={this.props.contact.firstName}/> 
         <input 
-          type = {'text'}
-          placeholder = {'Nazwisko'}
-          value = {this.props.contact.lastName}/>
+          type={'text'}
+          placeholder={'Nazwisko'}
+          value={this.props.contact.lastName}/>
         <input 
-          type = {'email'}
-          placeholder = {'Email'}
-          value = {this.props.contact.email}/> 
-        <button type = {'submit'}> 
+          type={'email'}
+          placeholder={'Email'}
+          value={this.props.contact.email}/> 
+        <button type={'submit'}> 
           {'Dodaj kontakt'} 
         </button> 
       </form >
